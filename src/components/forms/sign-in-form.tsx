b7e905962c7d874c8be7f5fc85f66bea5749e29b
Pedro Oliveira <pluiz@mychirotouch.com>
2022-12-01 23:12:06 -0300

    change form to antd

--- src/components/forms/sign-in-form.tsx
+++ src/components/forms/sign-in-form.tsx
@@ -1,65 +1,48 @@
-import { ErrorMessage, Field, Form, Formik, yupToFormErrors,  } from "formik"
+import { Form, Button, Input, Col } from "antd"
+import Title from "antd/es/typography/Title"
 import { useRouter } from "next/router"
 import { useSignIn } from "../../hooks/user-hooks"
-import { AxiosError } from "axios"
-import * as Yup from 'yup'
-
-const signInSchema = Yup.object().shape({
-    email: Yup.string().email('Must be a valid email').required('Required'),
-    password: Yup.string().min(3, 'Too short').required('Required')
-})
 
 export const SignInForm = () => {
     const { mutateAsync: signIn } = useSignIn()
     const { push } = useRouter()
 
+    const onFinish = (values: any) => {
+        signIn(values)
+        .then(() => {
+            localStorage.setItem('IS_LOGGED_IN', '1')
+            push('/')
+        })
+    }
+
     return (
-        <Formik initialValues={{
-            email: '',
-            password: ''
-        }}
-        validationSchema={signInSchema}
-        onSubmit={(values, helpers) => {
-            signIn(values)
-            .then(() => {
-                localStorage.setItem('IS_LOGGED_IN', '1')
-                push('/')
-            })
-            .catch(e => {
-                if (e instanceof AxiosError && e.response?.status === 404) {
-                    helpers.setErrors({
-                        email: 'User not found'
-                    })
-                } else {
-                    throw e
-                }
-            })
-        }}
+        <Form
+            initialValues={{ email: '', password: '' }}
+            onFinish={onFinish}
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 8 }}
         >
-            <Form className='flex flex-col space-y-2 w-1/2'>
-                <h1 className="mb-4 text-xl font-light">Login</h1>
-                <label htmlFor='email'>Email</label>
-                <Field className='border rounded p-2'
-                  id='email'
-                  name='email'
-                  type='email'/>
-                <ErrorMessage
-                  name='email'
-                  component={'p'}
-                  className='text-red-600'/> 
-                <label htmlFor='password'>Senha</label>
-                <Field className='border rounded p-2'
-                  id='password'
-                  name='password'
-                  type='password'/>
-                <ErrorMessage
-                  name='password'
-                  component={'p'}
-                  className='text-red-600'/> 
-                <button className='p-2 border rounded bg-blue-500 text-white'
-                  type='submit'>Entrar</button>
-            </Form>
-            
-        </Formik>
+            <Col span={8} offset={8}>
+                <Title>Login</Title>
+            </Col> 
+            <Form.Item
+              label='Email'
+              name='email'
+              rules={[{required: true, message: 'Required', whitespace: false }]}>
+                <Input type="email" />
+            </Form.Item>
+            <Form.Item
+                label="Senha"
+                name="password"
+                rules={[{ required: true, message: 'Please input your password!' }]}>
+                <Input.Password type="password"/>
+            </Form.Item>
+
+            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                <Button type="primary" htmlType="submit">
+                    Entrar
+                </Button>
+            </Form.Item>
+        </Form>
     )
 }
